302255b521506cab143fd9ff0ef635f6cf49ee05
sehee0531 <wjdtpgml0531@gmail.com>
2020-03-25 10:30:49 +0900

    별찍기1

---
+++ 별찍기1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	for (int a = 1; a <= 4; a++)
+	{
+
+		for (int b = 0; b < a; b++)
+		{
+
+
+			cout << "*";
+
+
+		}
+		cout << endl;
+
+
+	}
+}

80bd7ab764e4a844bbdb1aa8a5136133569cb547
sehee0531 <wjdtpgml0531@gmail.com>
2020-03-25 10:30:14 +0900

    1부터10까지 더한값

---
+++ 1부터10까지 더한값.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int i = 1;
+	while (i <= 10)
+	{
+
+		a = a + i;
+		i++;
+	}
+	cout << "1부터 10까지 더한 결과는" << a << "입니다.";
+}
+	
+

1ae1b335f02649916d2befce43346876e6c4d791
sehee0531 <wjdtpgml0531@gmail.com>
2020-04-18 17:15:58 +0900

    class 연습

---
+++ class 연습.cpp
@@ -0,0 +1,24 @@
+/*
+#include<iostream>
+
+using namespace std;
+
+class rectangle{
+public:
+	int width;
+	int height;
+	double sum();
+};
+double rectangle::sum(){
+	return width*height;
+
+}
+int main() {
+	rectangle rect;
+	rect.height = 3;
+	rect.width = 5;
+	double area = rect.sum();
+	cout << "사각형의 면적은 " << area << endl;
+
+}
+*/

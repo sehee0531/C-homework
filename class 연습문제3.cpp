ad8ad31df293f4d1380dfac070775032b2bff570
sehee0531 <wjdtpgml0531@gmail.com>
2020-04-21 15:59:34 +0900

    class 연습3

---
+++ class 연습문제3.cpp
@@ -0,0 +1,35 @@
+/*
+#include<iostream>
+
+using namespace std;
+
+class tower
+{
+public:
+	int height;
+	tower();
+	tower(int r);
+	int getHeight();
+
+};
+
+tower::tower() {
+	height = 1;
+}
+
+tower::tower(int r)
+{
+	height = r;
+}
+
+int tower::getHeight() {
+	return height;
+}
+
+int main() {
+	tower mytower;
+	tower seoultower(100);
+	cout << "높이는 " << mytower.getHeight() << "미터" << endl;
+	cout << "높이는 " << seoultower.getHeight() << "미터" << endl;
+}
+*/

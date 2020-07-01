78989e668ded7702479c403984a89ee0c7b2da12
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2020-07-01 22:18:18 +0900

    명품C++프로그래밍

---
+++ 5장_7번.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+
+using namespace std;
+
+class MyIntStack {
+	int p[10];
+	int tos;
+public:
+	MyIntStack();
+	bool push(int n);
+	bool pop(int &n);
+};
+
+int main() {
+	MyIntStack a;
+	for (int i = 0; i < 11; ++i) {
+		if (a.push(i)) cout << i << ' ';
+		else cout << endl << i + 1 << " 번째 stack full" << endl;
+	}
+	int n;
+	for (int i = 0; i < 11; ++i) {
+		if (a.pop(n))cout << n << ' ';
+		else cout << endl << i + 1 << " 번째 stack empty";
+	}
+	cout << endl;
+}

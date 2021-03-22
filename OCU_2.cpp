66ce3bc4fd758251adc7c213014b43f50aeee332
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-03-22 22:52:34 +0900

    Add files via upload

---
+++ OCU_2.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+int main(void) {
+
+	int x ;
+	int y ;
+
+	x = 20;
+	y = 10;
+
+	int sum1 = x + y;
+	int sum2 = x - y;
+	int sum3 = x * y;
+	int sum4 = x / y;
+
+	printf("두수의 합은 : %d\n ", sum1);
+	printf("두수의 차은 : %d\n ", sum2);
+	printf("두수의 곱은 : %d\n ", sum3);
+	printf("두수의 몫은 : %d\n ", sum4);
+
+	return 0;
+}

1a98b233eb5913022fc8bfce1400f88b62ed320f
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-04-04 16:01:38 +0900

    Add files via upload

---
+++ 나머지 연산자 예제.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define SEC_PER_MINUTE 60
+
+int main() {
+	int input, minute, second;
+	printf("초를 입력하세요");
+	scanf_s("%d", &input);
+
+	minute = input / SEC_PER_MINUTE;
+	second = input % SEC_PER_MINUTE;
+
+	printf("%d초는 %d분 %d초 입니다.", input, minute, second);
+
+	return 0;
+
+}

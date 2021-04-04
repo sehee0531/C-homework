f9abb083cd290011e3fec265765231f0e9b60ba1
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-04-04 16:01:19 +0900

    Add files via upload

---
+++ 정수 사칙 연산.cpp
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+
+int main(void) {
+
+	int x, y, result;
+
+	printf("두 정수를 입력하세요");
+	scanf_s("%d %d", &x, &y);
+
+	result = x + y;
+	printf("%d + %d = %d\n", x, y, result);
+
+	result = x - y;
+	printf("%d - %d = %d\n", x, y, result);
+
+	result = x * y;
+	printf("%d * %d = %d\n", x, y, result);
+
+	result = x / y;
+	printf("%d / %d = %d\n", x, y, result);
+
+	result = x % y;
+	printf("%d %% %d = %d\n", x, y, result);
+
+	return 0;
+}*/
